6c747fe7c8ff1e6a2dfd635d57214da95b83c9e3
Josef Koenig <josef_koenig@hotmail.com>
2026-02-23 17:39:28 +0100

    feat: implement severity sorting and PWA install button for mobile

---
+++ public/sw.js
@@ -0,0 +1,19 @@
+const CACHE_NAME = 'schuettel-dm-v1';
+const ASSETS = [
+    '/',
+    '/index.html',
+    '/favicon.png',
+    '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+});
